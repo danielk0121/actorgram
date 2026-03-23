03a3d5a76c3774bef6b629bf0253e40f4723b52a
danielk0121 <maedk10@gmail.com>
2026-03-23 23:28:59 +0900

    버전 정보를 상단바 2행 우측으로 이동

--- actorsapp/src/App.tsx
+++ actorsapp/src/App.tsx
@@ -35,10 +35,7 @@ function App() {
           <div className="header-row1">
             <NavLink to="/actors" className="header-left">
               <img src={`${import.meta.env.BASE_URL}favicon.svg`} alt="actors 로고" className="header-favicon" />
-              <div className="header-logo-wrap">
-                <div className="header-logo">actors</div>
-                <div className="header-version">v260323-2302</div>
-              </div>
+              <div className="header-logo">actors</div>
             </NavLink>
             <nav className="header-nav">
               <NavLink to="/actors" className={({ isActive }) => `nav-item${isActive ? ' nav-item--active' : ''}`}>배우</NavLink>
@@ -58,6 +55,7 @@ function App() {
               <NavLink to="/actor-detail" className={({ isActive }) => `nav-item${isActive ? ' nav-item--active' : ''}`}>배우 상세</NavLink>
               <NavLink to="/movie-detail" className={({ isActive }) => `nav-item${isActive ? ' nav-item--active' : ''}`}>영화 상세</NavLink>
             </nav>
+            <div className="header-version">v260323-2302</div>
           </div>
         </div>
       </header>
